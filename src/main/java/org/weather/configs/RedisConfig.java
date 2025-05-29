e32d0bdb5b6c8618598bd37af6bbca17aed7d79b
Danyl Paniutchev <yankee.democratic@gmail.com>
2025-05-29 20:46:52 +0300

    feat: Config for RedisTemplate

---
+++ src/main/java/org/weather/configs/RedisConfig.java
@@ -0,0 +1,22 @@
+package org.weather.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.weather.dtos.WeatherResponse;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, WeatherResponse> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, WeatherResponse> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
